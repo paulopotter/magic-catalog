bda6fb0c2833a7b6a1480d108be18f8d80d82d50
Paulo Oliveira <paulofrauches@gmail.com>
2015-02-07 14:20:53 -0200

    Adiciona a formatação de link de busca e corrige o dicionário com as informações das cartas.

--- parser_file.py
+++ parser_file.py
@@ -1,4 +1,6 @@
-# coding: UTF-8
+#! /usr/bin/env python
+# coding:utf-8
+
 from config import *
 from glob import glob
 
@@ -7,10 +9,7 @@ class ParserFile:
   def __init__(self):
     self.all_files = glob(PATH_FILES + "/*.txt")
     self.all_content = self.all_files_content
-
-
-    print self.all_content()
-
+    print self.find_card(self.list_content(self.all_files[0])[3]['card_name'])[0]
 
   def all_files_content (self):
     all_files = self.all_files
@@ -21,10 +20,28 @@ class ParserFile:
     return result
 
   def list_content(self, chosen_file):
+    result =[]
     with open(chosen_file) as f:
       content = f.readlines()
+    for file_line in range(len(content)):
+      line = content[file_line].strip()
+      line_split = line.split('[')
+      card_name = line_split[0]
+      card_quant = line_split[1]
+      result.append({
+          'card_name': card_name,
+          'quant': card_quant
+        })
 
-    return content
+    return result
+
+  def find_card(self, card_name):
+    sites = PAGE_CONFIG.keys()
+    result =[]
+    for page in sites:
+      result.append(PAGE_CONFIG[page]['search_format'] .format(card_name))
+
+    return result
 
 
 if __name__ == '__main__':
