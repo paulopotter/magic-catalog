88e6f91709730b90cf3eb4f8788b10f014d58946
Paulo Oliveira <paulofrauches@gmail.com>
2015-02-07 14:22:25 -0200

    Adiciona Configuração de pagina a ser buscada.

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 # coding: UTF-8
 
 PATH_FILES = 'listas/'
+PAGE_CONFIG = { 'ligamagic': {
+        'name': ' Liga Magic',
+        'url': 'http://ligamagic.com.br/',
+        'search_format': 'http://www.ligamagic.com.br/?view=cartas/card&card={0}',
+    }
+}
